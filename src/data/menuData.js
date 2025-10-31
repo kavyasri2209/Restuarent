f6b863f568757e661a90d75118c6dd8be46ca7cf
unknown <kavyasri336@gmail.com>
2025-11-01 02:19:38 +0530

    Updated components and fixed layout

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -16,6 +16,21 @@ import roti from "../Images/roti.jpg";
 import idliSambar from "../Images/idli-sambar.jpg";
 import kulfi from "../Images/kulfi.png";
 import rosefalooda from "../Images/rosefalooda.png";
+import matkaicecream from "../Images/matkaicecream.png";
+import mysorebajji from "../Images/mysorebajji.png";
+import puri from "../Images/puri.png"
+import dahivada from "../Images/dahivada.jpg";
+import uthappam from "../Images/uthappam.jpg";
+import pesarattu from "../Images/pesarattu.jpg";
+import vada from "../Images/vada.jpg";
+import masalatea from "../Images/masalatea.jpg";
+import coldcoffee from "../Images/coldcoffee.jpg";
+import limesoda from "../Images/limesoda.jpg";
+import lassi from "../Images/lassi.jpg";
+import filtercoffee from "../Images/filtercoffee.jpg"
+
+
+
 export const menuData = [
   {
     id: 1,
@@ -181,7 +196,7 @@ export const menuData = [
     id: 15,
     name: "Idli Sambar",
     category: "South Indian",
-    price: 100,
+    price: 60,
     description:
       "Steamed rice cakes served with lentil stew and chutney",
     image: idliSambar,
@@ -202,13 +217,141 @@ export const menuData = [
     available: true,
   },
   {
-    id: 16,
+    id: 17,
     name: "RoseFalooda",
     category: "Desserts",
-    price: 60,
+    price: 120,
     description:
       "Refreshing layered dessert drink with silky vermicelli, basil seeds, ice cream, and sweet syrup",
     image: rosefalooda,
+    vegetarian: true,
+    spicyLevel: 0,
+    available: true,
+  },
+  {
+    id: 18,
+    name: "MatkaIceCream",
+    category: "Desserts",
+    price: 220,
+    description:
+      "Creamy Matka Ice Cream served in a traditional clay pot for a rich, earthy flavor and authentic touch.",
+    image: matkaicecream,
+    vegetarian: true,
+    spicyLevel: 0,
+    available: true,
+  },
+    {
+    id: 19,
+    name: "Mysore Bajji",
+    category: "South Indian",
+    price: 70,
+    description:
+      "Fluffy, deep-fried savory fritters made from a tangy yogurt and flour batter, served with coconut chutney",
+    image: mysorebajji,
+    vegetarian: true,
+    spicyLevel: 1,
+    available: true,
+  },
+   {
+    id: 20,
+    name: "Puri",
+    category: "South Indian",
+    price: 150,
+    description:
+      "Soft, puffed puris served with a flavorful, spiced chickpeas curry for a comforting traditional meal.",
+    image: puri,
+    vegetarian: true,
+    spicyLevel: 1,
+    available: true,
+  },
+   {
+    id: 21,
+    name: "DahiVada",
+    category: "South Indian",
+    price: 180,
+    description:
+      "Soft lentil 3 pieces of  dumplings soaked in creamy yogurt, topped with tangy chutneys and aromatic spices for a refreshing treat",
+    image: dahivada,
+    vegetarian: true,
+    spicyLevel: 1,
+    available: true,
+  },
+   {
+    id: 22,
+    name: "Uthappam",
+    category: "South Indian",
+    price: 90,
+    description:
+      "Thick, savory South Indian pancake topped with onions, tomatoes, and chilies, served with chutney and sambar",
+    image: uthappam,
+    vegetarian: true,
+    spicyLevel: 1,
+    available: true,
+  },
+   {
+    id: 23,
+    name: "Pesarattu",
+    category: "South Indian",
+    price: 210,
+    description:
+      "Protein-rich green gram dosa served with flavorful upma inside, paired with chutney and ginger pickle for a wholesome mea",
+    image: pesarattu,
+    vegetarian: true,
+    spicyLevel: 1,
+    available: true,
+  },
+    {
+    id: 24,
+    name: "Vada",
+    category: "South Indian",
+    price: 70,
+    description:
+      "Crispy, golden South Indian fritters made from spiced lentil batter, served hot with chutney and sambar",
+    image: vada,
+    vegetarian: true,
+    spicyLevel: 1,
+    available: true,
+  },
+   {
+    id: 25,
+    name: "Masala Tea",
+    category: "Beverages",
+    price: 30,
+    description: "Aromatic Indian tea brewed with milk, spices, and sugar for a comforting traditional flavor.",
+    image: masalatea,
+    vegetarian: true,
+    spicyLevel: 0,
+    available: true,
+  },
+   {
+    id: 26,
+    name: "Cold Coffee",
+    category: "Beverages",
+    price: 100,
+    description: "Chilled, creamy blend of coffee, milk, and ice, topped with a touch of chocolate.",
+    image: coldcoffee,
+    vegetarian: true,
+    spicyLevel: 0,
+    available: true,
+  },
+   {
+    id: 27,
+    name: "Fresh Lime Soda",
+    category: "Beverages",
+    price: 40,
+    description: "Refreshing mix of lemon juice, soda, and sugar or salt for a tangy twist.",
+    image: limesoda,
+    vegetarian: true,
+    spicyLevel: 0,
+    available: true,
+  },
+   {
+    id: 28,
+    name: "Filter Coffee",
+    category: "Beverages",
+    price: 50,
+    description: "Strong South Indian coffee made with freshly brewed decoction and frothy milk.",
+    image: filtercoffee,
     vegetarian: true,
     spicyLevel: 0,
     available: true,
